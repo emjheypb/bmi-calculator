f0dfcc25e59b9505870de987fba7d5698f78463f
EmJhey <mjpb123@gmail.com>
2021-02-10 23:01:46 +0800

    Section 11 - 120, 121

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    
+    @IBOutlet weak var heightSldr: UISlider!
+    @IBOutlet weak var weightSldr: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func heightSldrChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLbl.text = "\(height)m"
+    }
+    
+    @IBAction func weightSldrChanged(_ sender: UISlider) {
+        let weight = round(sender.value * 100) / 100
+        weightLbl.text = "\(weight)Kg"
+    }
+    
+    @IBAction func calculateBMI(_ sender: UIButton) {
+        let height = heightSldr.value
+        let weight = weightSldr.value
+        let bmi = weight / pow(height, 2)
+        
+        print(String(format: "%.2f", bmi))
+    }
+    
 }
 
